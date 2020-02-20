1d5782f438f449e2637fdac0538d80b84e43c3fe
Jonathan Sztajnhart <jsztajnhart@tgv.com.ar>
2020-02-20 15:31:11 -0300

    First Commit

---
+++ ABMCursos/ABMCursos/ABMCursos/Models/ModuloModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ABMCursos.Models
+{
+    public class ModuloModel
+    {
+        public string NomModulo { get; set; }
+        public string descModulo { get; set; }
+    }
+}
---
+++ ABMCursos/ABMCursos/ABMCursos/ViewModels/BaseViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+using Xamarin.Forms;
+
+namespace ABMCursos.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public string Titulo { get; set; }
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            var changed = PropertyChanged;
+            if (changed == null)
+                return;
+
+            changed.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
